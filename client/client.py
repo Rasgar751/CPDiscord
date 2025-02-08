98bebd663d5e698e52de9d49e17c2bb1bb8fc523
Rasgar <118392474+Rasgar751@users.noreply.github.com>
2025-02-08 19:11:55 +0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,9 +1,9 @@
 import socket
 sock = socket.socket()
-sock.connect(('localhost', 9090))
-sock.send('hello world!')
+sock.connect(('localhost', 12345))
+sock.send('hello world!'.encode("utf-8"))
 
 data = sock.recv(1024)
 sock.close()
 
-print(data)
+print(data.decode())
