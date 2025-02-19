3a5bdcb869b1394c5bbdf2a73539833fbed327c4
Rasgar <rasgar751@gmail.com>
2025-02-19 19:16:49 +0700

    Общение между пользователями

--- client/client.py
+++ client/client.py
@@ -1,14 +1,26 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.connect(('localhost', 12345))
+# Функция для получения сообщений от сервера
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            print("Соединение с сервером потеряно.")
+            client_socket.close()
+            break
 
+# Настройка клиента
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 12345))
+
+# Запускаем поток для получения сообщений
+receive_thread = threading.Thread(target=receive_messages, args=(client,))
+receive_thread.start()
+
+# Отправка сообщений
 while True:
-    data = sock.recv(1024)
-    print("От сервера получены данные: ", data.decode())
-
     message = input()
-    print(f"Серверу отправлены данные: {message}")
-    sock.send(message.encode("utf-8"))
-
-    
+    client.send(message.encode('utf-8'))
--- server/server.py
+++ server/server.py
@@ -1,17 +1,36 @@
 import socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 12345))
+import threading
 
+# Функция для обработки подключений клиентов
+def handle_client(client_socket, addr):
+    while True:
+        try:
+            # Получаем сообщение от клиента
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                continue
+            print(f"Получено сообщение: {message}")
+            # Пересылаем сообщение всем клиентам
+            for client in clients:
+                client.send(f"Пользователь {addr} отправил сообщение: {message} ".encode('utf-8'))
+        except Exception as ex:
+            print("Попали в except")
+            print(ex)
+            continue
+
+# Настройка сервера
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 12345))
 server.listen()
-print("Сервер запущен и ждёт подключений")
-client, addr = server.accept()
+
+print("Сервер запущен и ожидает подключений...")
+
+clients = []
 
 while True:
-    print(f"Клиент подключился: {addr}")
-    
-    message = input()
-    client.send(message.encode("utf-8"))
-    print(f"Клиенту отправлены данные: {message}")
-
-    data = client.recv(1024)
-    print(f"От клиента получены данные: {data.decode()}")
+    client_socket, addr = server.accept()
+    print(f"Подключен клиент: {addr}")
+    clients.append(client_socket)
+    # Запускаем поток для обработки клиента
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+    client_thread.start()
