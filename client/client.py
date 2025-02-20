d4137a389ba4c1e85b53f6f64c18bf6b6cc5f14f
Rasgar <rasgar751@gmail.com>
2025-02-20 17:36:01 +0700

    Временное обновление

--- client/client.py
+++ client/client.py
@@ -1,16 +1,31 @@
 import socket
 import threading
+import json
+
+
+def is_authorized():
+    """Проверяет авторизован ли пользователь, 
+    если нет предоставляет выбор: зарегестироваться или авторизоватся,
+    сдесь же регистрируется или авторизуется.(Возможно функция немного перегруженна 0_0 )"""
+    with open('user.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)  
+        print(data)
 
 # Функция для получения сообщений от сервера
 def receive_messages(client_socket):
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
-            print(message)
+            if not message:
+                continue
+            else:
+                print(message)
         except:
             print("Соединение с сервером потеряно.")
             client_socket.close()
             break
+
+is_authorized()
 
 # Настройка клиента
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
